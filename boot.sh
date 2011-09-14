3c21f29517cfa3aa9cccb47c0466eb69c8b9415d
Rob Sherwood <rob.sherwood@bigswitch.com>
2011-09-14 08:45:41 -0700

    updates to build system for ease of use
    * made boot.sh auto grab and build submodules
    * added more deps to README
    * fixed submodule url to be non-password protectected

--- boot.sh
+++ boot.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
+echo Generating autoconf/automake goo...
 autoreconf -I m4 --install --force $@
+
+echo Downloading and building netfpga-packet-generator-c-library
+git submodule init && \
+	git submodule update && \
+	cd netfpga-packet-generator-c-library && \
+       	./autogen.sh && \
+       	./configure && \
+	make
